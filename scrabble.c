29b6d2ee6318f539e58e030c6d7e434c0de45fc6
Adeliya Tislova <143000049+tislova@users.noreply.github.com>
2024-02-07 14:35:05 -0500

    Add files via upload

---
+++ scrabble.c
@@ -0,0 +1,52 @@
+// Simulates scrabble game and identifies the winner
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int compute_score(char word[]);
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int main(void)
+{
+    char word1[100], word2[100];
+    printf("Player 1: ");
+    scanf("%s", word1);
+    printf("Player 2: ");
+    scanf("%s", word2);
+
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+    return 0;
+}
+
+int compute_score(char word[])
+{
+    int score = 0;
+
+    for (int i = 0, len = strlen(word); i < len; i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+
+    return score;
+}
